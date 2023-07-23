6d098306f424c341bfe3d3cc727286d401430ab2
Marvel Stardom <marvellousolaniyan@gmail.com>
2023-07-23 08:56:25 +0100

    updated

--- src/app/hooks/icons/page.js
+++ src/app/hooks/icons/page.js
@@ -1,14 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 export default function Icons() {
-    const [edit, setEdit] = useState(false);
-    const [deleteUser, setDeleteUser] = useState(false);
 
-    const handleDelete = (e) => {
-        // e.preventDefault();
-        // setDeleteUser(true);
-    }
     return (
     <div className='flex flex-row gap-2'>
         {/* EDIT */}
@@ -20,7 +14,7 @@ export default function Icons() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-teal-900 hover:bg-teal-700 text-white rounded-lg p-2 w-8 h-8"><path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" /></svg>
         </div>
         {/* DELETE */}
-        <button type='button' onClick={handleDelete()}>
+        <button type='button'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-rose-600 hover:bg-rose-500 text-white rounded-lg p-2 w-8 h-8"><path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
         </button>
     </div>
